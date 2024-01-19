01c053ce07f421122ed04c44e86fdb54c49ea99e
NasywaHanifa128@gmail.com <fatmafatimatuzzahra66@gmail.com>
2024-01-19 10:59:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fatma_jobshet12/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fatma_jobshet12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/fatma_jobshet12/praktikum_12.kt
@@ -0,0 +1,11 @@
+package com.example.fatma_jobshet12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_praktikum12 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_praktikum12)
+    }
+}
